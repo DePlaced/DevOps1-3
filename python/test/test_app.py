8d7d1082798654d279fb89b66b34597498a91751
DePlaced <117630280+DePlaced@users.noreply.github.com>
2025-02-19 13:32:36 +0100

    Update test_app.py

--- python/test/test_app.py
+++ python/test/test_app.py
@@ -1,28 +1,22 @@
-import unittest
 import sys
 import os
+import pytest
+
 sys.path.append(os.path.abspath("C:/Users/paapl/desktop/GIT/DevOps1-3/python"))
 from app import addition
 
-class TestAdditionFunction(unittest.TestCase):
-
-    def test_addition(self):
-        # Test with two positive integers
-        self.assertEqual(addition(3, 5), 8)
-        
-        # Test with negative integers
-        self.assertEqual(addition(-3, 5), 2)
-        
-        # Test with a zero
-        self.assertEqual(addition(0, 5), 5)
-        
-        # Test with floating point numbers
-        self.assertEqual(addition(3.5, 2.5), 6.0)
-        
-        # Test with zero and negative number
-        self.assertEqual(addition(-3, 0), -3)
-
-if __name__ == "__main__":
-    unittest.main()
-
-
+def test_addition():
+    # Test with two positive integers
+    assert addition(3, 5) == 8
+    
+    # Test with negative integers
+    assert addition(-3, 5) == 2
+    
+    # Test with a zero
+    assert addition(0, 5) == 5
+    
+    # Test with floating point numbers
+    assert addition(3.5, 2.5) == 6.0
+    
+    # Test with zero and negative number
+    assert addition(-3, 0) == -3
